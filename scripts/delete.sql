6b6047bec79bfa3182abbdaf695cf21caeff3596
sandbox9743 <noble.thomas1@outlook.com>
2024-03-02 01:10:29 -0700

    adding insert and delete script

---
+++ scripts/delete.sql
@@ -0,0 +1,8 @@
+DELETE FROM "Party" WHERE Name='thisismyparty';
+DELETE FROM "Race" WHERE Name='Elf';
+DELETE FROM "Class" WHERE Name='Paladin';
+DELETE FROM "Background" WHERE Name='Gambler';
+DELETE FROM "User" WHERE username='larry_bird';
+DELETE FROM "Character" WHERE Name='Larry';
+DELETE FROM "Skills" WHERE Name='Acrobatics';
+DELETE FROM "Saving_Throws" WHERE Name='My Saving Throw';
