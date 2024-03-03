a6ae7f5e74af7e4e09cf3ecb5f15ec64d60765d9
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-03-02 23:56:11 -0700

    Add utility to see all table contents

---
+++ scripts/show-all-tables.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Skills;
+SELECT * FROM Saving_Throws;
+SELECT * FROM Characters;
+SELECT * FROM Users;
+SELECT * FROM Party;
+SELECT * FROM Race;
+SELECT * FROM Class;
+SELECT * FROM Background;
+
