a5d9ad8892f683d4b3fbc2273aa9a7dd2d834f07
sandbox9743 <noble.thomas1@outlook.com>
2024-03-02 18:20:53 -0700

    updated insert and delete script to reflect changes in database-setup

--- scripts/insert.sql
+++ scripts/insert.sql
@@ -10,10 +10,10 @@ VALUES ('Paladin', '38');
 INSERT INTO "Background" (Name, Page_Number)
 VALUES ('Gambler', '39');
 
-INSERT INTO "User" (username, password)
+INSERT INTO "Users" (username, password)
 VALUES ('larry_bird', 'replace_this_with_hash');
 
-INSERT INTO "Character" (Name, Strength_Ability_Score, Dexterity_Ability_Score, Constitution_Ability_Score, Intelligence_Ability_Score, Wisdom_Ability_Score, Charisma_Ability_Score, Proficiency_bonus, username, BackgroundID, RaceID, ClassID)
+INSERT INTO "Characters" (Name, Strength_Ability_Score, Dexterity_Ability_Score, Constitution_Ability_Score, Intelligence_Ability_Score, Wisdom_Ability_Score, Charisma_Ability_Score, Proficiency_bonus, username, BackgroundID, RaceID, ClassID)
 VALUES ('Larry', '14', '15', '17', '55', '67', '1', '5', 'larry_bird', '1', '1', '1');
 
 INSERT INTO "Skills" (Name, Modifier, Proficiency, ID)
