fcecf9297a892b4b2f57075db23a25d474b3c320
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-03-02 23:50:52 -0700

    Fix foreign key identifiers

--- scripts/insert.sql
+++ scripts/insert.sql
@@ -13,11 +13,16 @@ VALUES ('Gambler', 39);
 INSERT INTO Users (username, password)
 VALUES ('larry_bird', 'replace_this_with_hash');
 
-INSERT INTO Characters (Name, Strength_Ability_Score, Dexterity_Ability_Score, Constitution_Ability_Score, Intelligence_Ability_Score, Wisdom_Ability_Score, Charisma_Ability_Score, Proficiency_bonus, username, BackgroundID, RaceID, ClassID)
-VALUES ('Larry', 14, 15, 17, 55, 67, 1, 5, 'larry_bird', 1, 1, 1);
+SET @BackgroundID = (SELECT BackgroundID FROM Background WHERE Name = 'Gambler');
+SET @RaceID = (SELECT RaceID FROM Race WHERE Name = 'Elf');
+SET @ClassID = (SELECT ClassID FROM Class WHERE Name = 'Paladin');
 
+INSERT INTO Characters (Name, Strength_Ability_Score, Dexterity_Ability_Score, Constitution_Ability_Score, Intelligence_Ability_Score, Wisdom_Ability_Score, Charisma_Ability_Score, Proficiency_bonus, username, BackgroundID, RaceID, ClassID)
+VALUES ('Larry', 14, 15, 17, 55, 67, 1, 5, 'larry_bird', @BackgroundID, @RaceID, @ClassID);
+
+SET @CharacterID = (SELECT ID FROM Characters WHERE Name = 'Larry');
 INSERT INTO Skills (Name, Modifier, Proficiency, ID)
-VALUES ('Acrobatics', 8, True, 2);
+VALUES ('Acrobatics', 8, True, @CharacterID);
 
 INSERT INTO Saving_Throws (Name, Modifier, Proficiency, ID)
-VALUES ('My Saving Throw', 6, False, 14);
+VALUES ('My Saving Throw', 6, False, @CharacterID);
