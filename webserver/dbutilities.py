91ad71bb451a799f9fe58f119a7be077a8a90b7f
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-03-30 13:53:58 -0600

    Add queries needed for logging in

--- webserver/dbutilities.py
+++ webserver/dbutilities.py
@@ -1,15 +1,23 @@
 """
-Contains functions that are utilized by the main flask application
+Contains only functions that interact with the database
 """
+import os
 import mariadb
+from dotenv import load_dotenv
 
-def initialize_connection(dbuser, dbpassword, dbhost, dbport, dbname):
+def initialize_connection():
     """
     Initializes connection to database
 
     Returns:
         mariadb.connections.Connection: Object for interacting with the database
     """
+    load_dotenv()
+    dbuser = os.getenv('dbuser')
+    dbpassword = os.getenv('dbpassword')
+    dbhost = os.getenv('dbhost')
+    dbport = os.getenv('dbport')
+    dbname = os.getenv('dbname')
     try:
         connection = mariadb.connect(
             user=dbuser,
@@ -22,3 +30,67 @@ def initialize_connection(dbuser, dbpassword, dbhost, dbport, dbname):
         print(f"Error connecting to MariaDB Platform: {e}")
     return connection
 
+def get_table_contents(table_name):
+    """
+    Gets contents of a particular table
+
+    Returns:
+        Result of query
+    """
+    conn = initialize_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM %s"
+    cursor.execute(query, (table_name,))
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def is_user(username) -> bool:
+    """
+    Gets all usernames
+    """
+    conn = initialize_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT username from Users WHERE username = %s;"
+    cursor.execute(query, (username))
+    if cursor.rowcount > 0:
+        conn.close()
+        return True
+    conn.close()
+    return False
+
+def create_user(username, hashed_password):
+    """
+    Adds a user to the Users table if they don't already exist
+    """
+    conn = initialize_connection()
+    cursor = conn.cursor()
+
+    query = "INSERT INTO Users (username, password) VALUES (%s, %s);"
+    cursor.execute(query, (username, hashed_password))
+
+    conn.commit()
+    rows_affected = cursor.rowcount
+    conn.close()
+
+    return rows_affected > 0
+
+def get_password_hash(username) -> str:
+    """
+    Gets the stored password hash for a user.
+    """
+    conn = initialize_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT password FROM Users WHERE username = %s;"
+    cursor.execute(query, (username,))
+    result = cursor.fetchall()
+
+    conn.close()
+
+    if result:
+        return result[0][0]
+    else:
+        return None
